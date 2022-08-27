a3e0afeedf0bc5259470b3f6ae429299eb7b8c74
Tomi Tokko <56736462+tomitokko@users.noreply.github.com>
2022-08-27 16:00:06 +0100

    Create word replacement.py

---
+++ word replacement.py
@@ -0,0 +1,7 @@
+def replace_word():
+    str = "hi guys, i am tomi, and hi hi hi hi"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+    
+replace_word()
