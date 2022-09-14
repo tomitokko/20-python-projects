b78b189d1285e5d57effb5b1aff37216837d653a
FitriRibbit <91612800+FitriRibbit@users.noreply.github.com>
2022-09-14 07:32:48 -0700

    Update word replacement.py
    Made some change

--- word replacement.py
+++ word replacement.py
@@ -1,7 +1,10 @@
 def replace_word():
-    str = "hi guys, i am tomi, and hi hi hi hi"
-    word_to_replace = input("Enter the word to replace: ")
+    str = input("Words i wanna type: ") #made word with input from user
+    word_to_replace = input("Enter The Word to replace: ")
     word_replacement = input("Enter the word replacement: ")
     print(str.replace(word_to_replace, word_replacement))
-    
+    back = input("Still wanna play this game : (y/n) ? ")
+    if back == 'y' or back == 'Y': #made replay game if input = y or Y
+        return replace_word()
+        
 replace_word()
