974101096d815be53adf423530b4ee9eee79cada
FitriRibbit <91612800+FitriRibbit@users.noreply.github.com>
2022-09-15 08:10:33 -0700

    Update email slicer.py
    Made some code to replay game

--- email slicer.py
+++ email slicer.py
@@ -1,16 +1,19 @@
 def main():
-    print(" Welcome to the email slicer ")
-    print("")
-
     email_input = input("Input your email address: ")
 
     (username, domain) = email_input.split("@")
     (domain, extension) = domain.split(".")
 
-    print("Username : ", username)
-    print("Domain : ", domain)
-    print("Extension: ", extension)
+    print("Username :" , username)
+    print("Domain :", domain)
+    print("Extension :", extension)
 
-    
-while True:
-    main()
+    print("")
+    replay = input("Still Wanna Play ? (y/n) ") # Ask Wanna PLay again ? Give option to continue play or not
+    if replay == "Y" or replay == "y":
+        main()
+
+print("Welcome to the email slicer ")
+print("")
+
+main()
