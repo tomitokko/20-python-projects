63b83fb7909c00cd7b48aaa276ab0c79da9c9ffa
FitriRibbit <91612800+FitriRibbit@users.noreply.github.com>
2022-09-16 07:32:15 -0700

    Update quiz program.py
    Add some option to replay game again

--- quiz program.py
+++ quiz program.py
@@ -1,49 +1,65 @@
-quiz = {
-    "question1": {
-        "question": "What is the capital of France?",
-        "answer": "Paris"
-    },
-    "question2": {
-        "question": "What is the capital of Germany?",
-        "answer": "Berlin"
-    },
-    "question3": {
-        "question": "What is the capital of Italy?",
-        "answer": "Rome"
-    },
-    "question4": {
-        "question": "What is the capital of Spain?",
-        "answer": "Madrid"
-    },"question5": {
-        "question": "What is the capital of Portugal?",
-        "answer": "Lisbon"
-    },"question6": {
-        "question": "What is the capital of Switzerland?",
-        "answer": "Bern"
-    },"question7": {
-        "question": "What is the capital of Austria?",
-        "answer": "Vienna"
-    },
-}
+def ask():
+    quiz = {
+        "question1" : {
+            "question": "What is the capital of France?",
+            "answer": "Paris"
+        },
+        "question2" : {
+            "question": "What is the capital of Germany?",
+            "answer": "Berlin"
+        },
+        "question3" : {
+            "question": "What is the capital of Italy?",
+            "answer": "Roma"
+        },
+        "question4" : {
+            "question": "What is the capital of Spain?",
+            "answer": "Madrid"
+        },
+        "question5" : {
+            "question": "What is the capital of Portugal?",
+            "answer": "Lisbon"
+        },
+        "question6" : {
+            "question": "What is the capital of Switzerland?",
+            "answer": "Bern"
+        },
+        "question7" : {
+            "question": "What is the capital of Austria?",
+            "answer": "Vienna"
+        },
+    
+    }
 
-score = 0
+    score = 0
 
-for key, value in quiz.items():
-    print(value[' question' ])
-    answer = input("Answer? ")
+    for key, value in quiz.items():
+        print(value['question'])
+        answer = input("Answer? ")
+    
+        if answer.lower() == value['answer'].lower():
+            print('Correct')
+            score =  score + 1
+            print("Your Score: " + str(score))
+            print("")
+            print("")
+        else:
+            print("Wrong!")
+            print("The Answer is : " + value['answer'])
+            print("Your Score is : " + str(score))
+            print("")
+            print("")
 
-    if answer.lower() == value[ 'answer'] . lower():
-        print('Correct')
-        score = score + 1
-        print("Your score is: " + str(score))
-        print("")
-        print("")
-    else:
-        print("Wrong!")
-        print("The answer is : " + value[ ' answer'])
-        print("Your score is: " + str(score))
-        print("")
-        print("")
+    print("You got " + str(score) + " out of 7 question correctly")
+    print("Your percentage is " + str(int(score/7 * 100)) + " %")
 
-print("You got " + str(score) + " out of 7 questions correctly")
-print("Your percentage is " + str(int(score/7 * 100)) + "%")
+
+    while True:
+        replay = input("Still Wanna Play ? (y/n) ")
+        if replay == "Y" or replay == "y":
+            ask()
+        else:
+            quit()
+
+print("Welcome to Our Quiz!!")
+ask()
