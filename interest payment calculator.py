64e29afda2df36889364cf80e4c09ad0f1cb8b78
FitriRibbit <91612800+FitriRibbit@users.noreply.github.com>
2022-09-16 07:36:13 -0700

    Update interest payment calculator.py
    Add option replay game

--- interest payment calculator.py
+++ interest payment calculator.py
@@ -1,15 +1,20 @@
 def main():
-    print(" This is a monthly payment loan calculator ")
+    print("This is monthly loan calculator")
     print("")
-    
-    principal = float(input("Input the loan amount: "))
-    apr = float(input("Input the annual interest rate: "))
-    years = int(input("Input amount of years: "))
-    
+
+    principal = float(input("Input The Loan Amount: "))
+    apr = float(input("Input The Annual Interest Rate: "))
+    years = int(input("Input Amount Of Years: "))
+
     monthly_interest_rate = apr / 1200
     amount_of_months = years * 12
-    monthly_payment = principal * monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-amount_of_months))
-    
-    print(" The monthly payment for this loan is: %.2f " % monthly_payment)
+    monthly_payment = principal * monthly_interest_rate / (1-(1 + monthly_interest_rate) ** (-amount_of_months))
+
+    print("The monthly payment for this loan is : $ %.2f " % monthly_payment)
+
+    play = input("Still Wanna Count? (y/n) ") # made some replay count again
+    if play == "Y" or play == "y":
+        main()
 
 main()
+
